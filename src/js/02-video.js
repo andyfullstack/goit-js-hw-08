e9051097a26a6aaa05e67e8cb6ee97c99c4098fc
andyfullstack <andriikoliadenko@gmail.com>
2023-10-16 16:07:37 +0300

    form compld

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Vimeo from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const player = new Vimeo('vimeo-player');
+// position Save
+player.on(
+  'timeupdate',
+  throttle(() => {
+    player.getCurrentTime().then(time => {
+      const minutes = Math.floor(time / 60);
+      const seconds = Math.floor(time % 60);
+      const goodTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+
+      localStorage.setItem('videoplayer-current-time', goodTime);
+    });
+  }, 500)
+);
+
+// Request at position
+const saveTime = localStorage.getItem('videoplayer-current-time');
+
+if (saveTime) {
+  player.setCurrentTime(saveTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import { throttle } from 'lodash';
+// Import throttle from lodash db
 
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('[name="email"]');
+const messageInput = feedbackForm.querySelector('[name="message"]');
+const sendBtn = feedbackForm.querySelector('button[type="submit"]');
+sendBtn.disabled = true;
+
+//Disable empty spaces
+
+feedbackForm.addEventListener('input', function () {
+  sendBtn.disabled = emailInput.value === '' || messageInput.value === '';
+});
+
+//Save to LocalStorage every 1 sec
+const saveFormState = throttle(() => {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}, 500);
+
+emailInput.addEventListener('input', saveFormState);
+messageInput.addEventListener('input', saveFormState);
+//Got from LocalStorage
+const savedState = localStorage.getItem('feedback-form-state');
+
+if (savedState) {
+  const parsedState = JSON.parse(savedState);
+  emailInput.value = parsedState.email;
+  messageInput.value = parsedState.message;
+}
+//Cleaned space and LS
+feedbackForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  console.log(formState);
+
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+  sendBtn.disabled = true;
+});
